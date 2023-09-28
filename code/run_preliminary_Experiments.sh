4740aecc340c534ec3b6b9cabdd03338840517b0
Weiwei Sun <68775773+sunnweiwei@users.noreply.github.com>
2023-09-29 02:26:32 +0800

    Update run_preliminary_Experiments.sh

--- code/run_preliminary_Experiments.sh
+++ code/run_preliminary_Experiments.sh
@@ -3,22 +3,22 @@ root_path=/home/jtj/probing_llama
 
 for (( n=0 ; n<=49 ; n++ ))
 do
-    python interpret_with_counterfact.py --config_yaml ${root_path}/experiments/commonsense/process/process_Llama-2-7b-chat-hf.yaml --fact_idx $n
-    python interpret_with_counterfact.py --config_yaml ${root_path}/experiments/commonsense/process/process_Llama-2-13b-chat-hf.yaml --fact_idx $n
-    python interpret_with_counterfact.py --config_yaml ${root_path}/experiments/commonsense/process/process_Llama-2-70b-chat-hf.yaml --fact_idx $n
-done
+    python interpret_with_counterfact.py --config_yaml ${root_path}/experiments/commonsense/process/process_Llama-2-7b-chat-hf.yaml --fact_idx $n --root_path ${root_path}
+    python interpret_with_counterfact.py --config_yaml ${root_path}/experiments/commonsense/process/process_Llama-2-13b-chat-hf.yaml --fact_idx $n --root_path ${root_path}
+    python interpret_with_counterfact.py --config_yaml ${root_path}/experiments/commonsense/process/process_Llama-2-70b-chat-hf.yaml --fact_idx $n --root_path ${root_path}
+done 
 
-python interpret_with_password.py --config_yaml ${root_path}/experiments/password/process/process_Llama-2-7b-chat-hf.yaml
-python interpret_with_password.py --config_yaml ${root_path}/experiments/password/process/process_Llama-2-13b-chat-hf.yaml
-python interpret_with_password.py --config_yaml ${root_path}/experiments/password/process/process_Llama-2-70b-chat-hf.yaml
+python interpret_with_password.py --config_yaml ${root_path}/experiments/password/process/process_Llama-2-7b-chat-hf.yaml --root_path ${root_path}
+python interpret_with_password.py --config_yaml ${root_path}/experiments/password/process/process_Llama-2-13b-chat-hf.yaml --root_path ${root_path}
+python interpret_with_password.py --config_yaml ${root_path}/experiments/password/process/process_Llama-2-70b-chat-hf.yaml --root_path ${root_path}
 
 for (( n=0 ; n<=49 ; n++ ))
 do
-    python interpret_with_counterfact.py --config_yaml ${root_path}/experiments/commonsense/process/process_Llama-2-7b-hf.yaml --fact_idx $n
-    python interpret_with_counterfact.py --config_yaml ${root_path}/experiments/commonsense/process/process_Llama-2-13b-hf.yaml --fact_idx $n
-    python interpret_with_counterfact.py --config_yaml ${root_path}/experiments/commonsense/process/process_Llama-2-70b-hf.yaml --fact_idx $n
+    python interpret_with_counterfact.py --config_yaml ${root_path}/experiments/commonsense/process/process_Llama-2-7b-hf.yaml --fact_idx $n --root_path ${root_path}
+    python interpret_with_counterfact.py --config_yaml ${root_path}/experiments/commonsense/process/process_Llama-2-13b-hf.yaml --fact_idx $n --root_path ${root_path}
+    python interpret_with_counterfact.py --config_yaml ${root_path}/experiments/commonsense/process/process_Llama-2-70b-hf.yaml --fact_idx $n --root_path ${root_path}
 done
 
-python interpret_with_password.py --config_yaml ${root_path}/experiments/password/process/process_Llama-2-7b-hf.yaml
-python interpret_with_password.py --config_yaml ${root_path}/experiments/password/process/process_Llama-2-13b-hf.yaml
-python interpret_with_password.py --config_yaml ${root_path}/experiments/password/process/process_Llama-2-70b-hf.yaml
+python interpret_with_password.py --config_yaml ${root_path}/experiments/password/process/process_Llama-2-7b-hf.yaml --root_path ${root_path}
+python interpret_with_password.py --config_yaml ${root_path}/experiments/password/process/process_Llama-2-13b-hf.yaml --root_path ${root_path}
+python interpret_with_password.py --config_yaml ${root_path}/experiments/password/process/process_Llama-2-70b-hf.yaml --root_path ${root_path}
