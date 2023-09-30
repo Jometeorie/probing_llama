c0ea49531778b87b65928c4abfd81e721081a4d7
Weiwei Sun <68775773+sunnweiwei@users.noreply.github.com>
2023-09-30 20:52:11 +0800

    Update run_preliminary_Experiments.sh

--- code/run_preliminary_Experiments.sh
+++ code/run_preliminary_Experiments.sh
@@ -19,6 +19,6 @@ do
 #    python interpret_with_counterfact.py --config_yaml ${root_path}/experiments/commonsense/process/process_Llama-2-70b-hf.yaml --fact_idx $n --root_path ${root_path}
 done
 
-python interpret_with_password.py --config_yaml ${root_path}/experiments/password/process/process_Llama-2-7b-hf.yaml --root_path ${root_path} --root_path ${root_path}
+python interpret_with_password.py --config_yaml ${root_path}/experiments/password/process/process_Llama-2-7b-hf.yaml --root_path ${root_path}
 python interpret_with_password.py --config_yaml ${root_path}/experiments/password/process/process_Llama-2-13b-hf.yaml --root_path ${root_path}
 #python interpret_with_password.py --config_yaml ${root_path}/experiments/password/process/process_Llama-2-70b-hf.yaml --root_path ${root_path}
