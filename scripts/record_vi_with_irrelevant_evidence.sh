9269cf28102e4299feea1ad8cb996d8ce131c8c6
Jometeorie <1252742119@qq.com>
2023-10-04 21:49:03 +0800

    Update record_vi_with_irrelevant_evidence.sh

--- scripts/record_vi_with_irrelevant_evidence.sh
+++ scripts/record_vi_with_irrelevant_evidence.sh
@@ -3,7 +3,7 @@
 # conflicting knowledge部分各层Vi热力图
 root_path=/home/jtj/probing_llama
 
-for (( n=0; n<=10; i++ ))
+for (( n=0; n<=10; n++ ))
 do
     # python ${root_path}/code/interpret_context_knowledge.py \
     #     --config_yaml ${root_path}/experiments/vi_with_irr_evidence/Llama-2-7b-chat-hf.yaml --is_probing True --is_record_last_vi True --root_path ${root_path} --fact_idx -1 --num_of_irrelevant_evidence $n
